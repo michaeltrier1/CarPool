f7374c5f5dc91d1c9a8dfbb9d4dfc3fa12ec0b6f
DESKTOP-POSUAP3\Michael <fartillucas@gmail.com>
2019-10-30 06:28:53 +0100

    model

---
+++ src/model/posts.js
@@ -0,0 +1,23 @@
+app.get('/',function(req, res){
+
+res.sendFile(path.join(__dirname+'/posts.html'));
+});
+app.post('/submit',function(req,res){
+
+  var title=req.body.title;
+  var text=req.body.text;
+  var fromLoaction=req.body.fromLoaction;
+  var toLoaction=req.body.toLocation;
+
+
+  connect.connect(function(err){
+    if (err) throw err;
+    console.log("Connected");
+var sql = "INSERT INTO posts (title, text,fromLoaction, toLoaction) VALUES ('"+title+"', '"+text+"','"+fromLoaction+"','"+toLoaction+"')";  });
+con.query(sql, function(err,result){
+  if (err) throw err;
+  console.log("post succesfully inserted");
+  res.end();
+
+});
+};
