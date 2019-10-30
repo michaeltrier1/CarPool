fac83e663bfb37fc2c2c78036e5a5e42af416fc7
HCHB <32193248+HCHB@users.noreply.github.com>
2019-10-30 08:15:55 +0100

    something

--- src/model/users.js
+++ src/model/users.js
@@ -1,22 +1,20 @@
 app.get('/',function(req, res){
-
-res.sendFile(path.join(__dirname+'/index.html'));
+    res.sendFile(path.join(__dirname+'/index.html'));
 });
 app.post('/submit',function(req,res){
+    var username=req.body.username;
+    var email=req.body.email;
+    var password=req.body.password;
 
-  var username=req.body.username;
-  var email=req.body.email;
-  var password=req.body.password;
+    connect.connect(function(err){
+      if (err) throw err;
+      console.log("Connected");
+      var sql = "INSERT INTO users (username, email,password) VALUES ('"+username+"', '"+email+"','"+password+"')";  
+    });
 
-
-  connect.connect(function(err){
-    if (err) throw err;
-    console.log("Connected");
-var sql = "INSERT INTO users (username, email,password) VALUES ('"+username+"', '"+email+"','"+password+"')";  });
-con.query(sql, function(err,result){
-  if (err) throw err;
-  console.log("user succesfully inserted");
-  res.end();
-
+    con.query(sql, function(err,result){
+        if (err) throw err;
+        console.log("user succesfully inserted");
+        res.end();
+    });
 });
-};
