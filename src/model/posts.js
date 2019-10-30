7f7b0d868bc2fed6718c7168778f0cc46f41fc32
HCHB <32193248+HCHB@users.noreply.github.com>
2019-10-30 09:30:24 +0100

    PostModel
    making postmodel a class

--- src/model/posts.js
+++ src/model/posts.js
@@ -1,20 +1,44 @@
-var connect = require('./src/core/database.js')
+Class postModel {
+
+    var connect = require('./src/core/database.js');
+    
+    Constructor(){ 
+    }
+    
+    savePost(title, text, fromLocation, toLocation){
+/*        var title=req.body.title;
+        var text=req.body.text;
+        var fromLoaction=req.body.fromLocation;
+        var toLoaction=req.body.toLocation;
+*/
+        
+        connect.connect(function(err){
+            if (err) throw err;
+            console.log("Connected");
+            var sql = "INSERT INTO posts (title, text,fromLoaction, toLoaction) VALUES ('"+title+"', '"+text+"','"+fromLocation+"','"+toLocation+"')";  });
+        
+        con.query(sql, function(err,result){
+            if (err) throw err;
+            console.log("post succesfully inserted");
+            res.end();
+        });
+    }
+    
+    loadPost(startNumber, endNumber){
+        connect.connect(function(err){
+            if (err) throw err;
+            console.log("Connected");
+            var sql = "Select INTO posts (title, text,fromLoaction, toLoaction) VALUES ('"+title+"', '"+text+"','"+fromLocation+"','"+toLocation+"')";  });
+        
+        con.query(sql, function(err,result){
+            if (err) throw err;
+            console.log("post succesfully inserted");
+            res.end();
+        });
+    }
+        
+}
+    
 
 
-  var title=req.body.title;
-  var text=req.body.text;
-  var fromLoaction=req.body.fromLocation;
-  var toLoaction=req.body.toLocation;
-
-
-  connect.connect(function(err){
-    if (err) throw err;
-    console.log("Connected");
-var sql = "INSERT INTO posts (title, text,fromLoaction, toLoaction) VALUES ('"+title+"', '"+text+"','"+fromLoaction+"','"+toLocation+"')";  });
-con.query(sql, function(err,result){
-  if (err) throw err;
-  console.log("post succesfully inserted");
-  res.end();
-
-});
-});
+module.exports = new postModel();
