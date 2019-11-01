ed74b0cc4f82787103997cee60317aa5f44e102d
Michael poulsen <fartillucas@gmail.com>
2019-11-01 12:49:52 +0100

    authuser

--- src/model/users.js
+++ src/model/users.js
@@ -2,13 +2,13 @@ class userModel{
 
     Constructor(){
     }
-    
+
     saveUser(username, email, password){
         //console.log("entered saveUser")
         let path = require('path');
         let databaseCore = require(path.join(__dirname, '../core/database.js'));
         //console.log("before entering databaseCore connect")
-        
+
         databaseCore.connect((client) => {
             //console.log("inside insertUser")
             let sqlString = "INSERT INTO users(username, email, password) VALUES('"+username+"','"+email+"','"+password+"')";
@@ -19,8 +19,8 @@ class userModel{
                 client.end();
             });
         });
-    }   
-    
+    }
+
     getUsers(){
         let databaseCore = require('./core/database.js');
 
@@ -30,8 +30,18 @@ class userModel{
             client.end()
         });
     }
-    
-    
+
+    authUser(){
+        let databaseCore = require('./core/database.js');
+
+        client.query('SELECT * from users where username=('"+username+"','"password"', (err, res) =>{
+            console.log('inside query');
+            console.log(res)
+            client.end()
+        });
+    }
+
+
 }
-         
+
 module.exports = new userModel();
