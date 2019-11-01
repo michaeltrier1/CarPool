edd6468a81e7b91ea61ff2b1830cd712c98e64ce
Michael poulsen <fartillucas@gmail.com>
2019-11-01 08:29:18 +0100

    shit

--- databaseTest.js
+++ databaseTest.js
@@ -1,3 +1,4 @@
+
 var pg = require('pg');
 
 var connectionString = 'postgres://rqudjcey:0XBP0dAZ7j39d5Gu3nYuC3xl95Rz-Hr0@balarama.db.elephantsql.com:5432/rqudjcey'
@@ -9,11 +10,11 @@ var client = new pg.Client(connectionString);
 
 
 client.connect(function(err){
-  
-    
+
+
     client.query('select * from users', function(err, result){
         console.log('inside query');
         console.log(result)
-        client.end();  
+        client.end();
     });
 });
--- src/model/users.js
+++ src/model/users.js
@@ -1,35 +1,18 @@
 class userModel{
 
-  var connect = require('./src/core/database.js');
 
 
   saveUser(username, email, password){
 
-    connect.connect(function(err){
-      if (err) throw err;
+    client.connect(function(err){
+
       console.log("Connected");
-      var sql = "INSERT INTO users (username, email,password) VALUES ('"+username+"', '"+email+"','"+password+"')";
+      client.query('INSERT INTO users(username, email, password)VALUES("derp","derpmail2","derpypw")',(err, res){
+        console.log('inside query');
+        console.log(res)
+        client.end();
+      });
     });
 
-    con.query(sql, function(err,result){
-        if (err) throw err;
-        console.log("user succesfully inserted");
-        res.end();
-    });
-
-    deleteUser(userid){
-      connect.connect(function(err){
-        if (err) throw err;
-        console.log("Connected");
-        var sql = "DELETE FROM users WHERE userid='?'";
-      });
-
-      con.query(sql, function(err,result){
-          if (err) throw err;
-          console.log("user succesfully deleted");
-          res.end();
-      });
-
-    }
 }
-module.exports= new userModel();
+        
