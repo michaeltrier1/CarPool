9ea0db780f8e3ff9ac27f635d23ba869a4f14bf7
HCHB <32193248+HCHB@users.noreply.github.com>
2019-11-05 13:06:03 +0100

    Search posts
    Search posts backend should be done

--- src/model/posts.js
+++ src/model/posts.js
@@ -19,7 +19,6 @@ class postModel {
     getPosts(startPost, numberOfPosts, callback){          
         let path = require('path');
         let databaseCore = require(path.join(__dirname,'../core/database.js'));
-        let success = false;
 
         databaseCore.connect((client) => {
             client.query("SELECT * from posts ORDER BY timestamp DESC LIMIT "+numberOfPosts+" OFFSET "+startPost, (err, res) =>{
@@ -41,6 +40,33 @@ class postModel {
                 callback(packagedResults)
             });
         })
+    }
+    
+    
+    
+    searchPosts(from, to, callback){  
+        let path = require('path');
+        let databaseCore = require(path.join(__dirname,'../core/database.js'));
+
+        databaseCore.connect((client) => {
+            client.query("SELECT * from posts WHERE tolocation = "+to+" AND fromlocaton = "+from+" ORDER BY timestamp DESC", (err, res) =>{
+                let results = res.rows;
+                let packagedResults = []
+                results.forEach(function(row) {
+                   let packagedRow = {
+                       "title": row.title,
+                       "text": row.text,
+                       "from": row.fromlocation,
+                       "to": row.tolocation,
+                       "userid": row.userid,
+                       "postid": row.postid
+                   }
+                   packagedResults.push(packagedRow)
+                });
+                
+                client.end();
+                callback(packagedResults)
+            });
     }
 }
 
