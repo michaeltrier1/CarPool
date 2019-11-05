ae9359d4a2caec7db491baac112df9770c7a76f3
HCHB <32193248+HCHB@users.noreply.github.com>
2019-11-05 13:47:50 +0100

    SessionManager
    removed some unused functionality

--- SessionController.js
+++ SessionController.js
@@ -1,90 +1,68 @@
-var userRoles = {
-  Admin: "administrator",
-  User: "user",
-  Supervisor: "supervisor"
-};
 var sessions = [];
-
+/*
 function(sessionId){
-  for(var i in sessions){
-    if(sessions[i].sessionId == sessionId)
-      return i;
-  }
-}
+    for(var i in sessions){
+        if(sessions[i].sessionId == sessionId)
+            return i;
+    }
+}*/
 
 var sessionController = {
-  indexOf: function(sessionId){
-    for(var i in sessions){
-      if(sessions[i].sessionId == sessionId)
-        return i;
-    }
-    return null;
-  },
-  indexOfUser: function(userId) {
-    for(var i in sessions){
-      if (sessions[i].userId == userId)
-        return i;
-    }
-    return null;
-  },
-add: function(sessionData){
-  sessions.push(sessionData);
-},
-remove: function(sessionId){
-  var index = this.indexOf(sessionId);
-  if(index != null){
-    sessions.splice(index, 1);
-  } else {
-    return null;
-  }
-},
-removeByUserId: function(userId){
-  var index = this.indexOf(userId);
-  if(index !=null){
-    sessions.splice(index, 1);
-  } else {
-    return null;
-  }
-},
-
-  getSessionById: function(userId) {
-    var index = this.indexOfUser(userId);
-    if(index != null) {
-        return sessions[index];
-    } else {
-        return null;
-    }
-  },
-  getSessionByUserId: function(sessionId) {
-    var index = this.indexOfUser(userId);
-    if(index != null) {
-        return sessions[index];
-    } else {
-        return null;
-    }
-  },
-
-  isAdmin: function(userId) {
-    var index = this.indexOfUser(userId);
-    if(index != null) {
-        if(users[index].role == userRoles.Admin) {
-            return true;
-        } else {
-            return false;
+    indexOf: function(sessionId){
+        for(var i in sessions){
+            if(sessions[i].sessionId == sessionId)
+                return i;
         }
-    } else {
         return null;
-    }
-  },
-  getUsersByRole: function(role) {
-    var usersByRole = [];
-    for(var i in users) {
-        if(users[i].role == role)
-            usersByRole.push(users[i]);
-    }
+    },
+    
+    indexOfUser: function(userId) {
+        for(var i in sessions){
+            if (sessions[i].userId == userId)
+                return i;
+        }
+        return null;
+    },
 
-    return usersByRole;
-  }
+    add: function(sessionData){
+        sessions.push(sessionData);
+    },
+    
+    remove: function(sessionId){
+        var index = this.indexOf(sessionId);
+        if(index != null){
+            sessions.splice(index, 1);
+        } else {
+            return null;
+        }
+    },
+    
+    removeByUserId: function(userId){
+        var index = this.indexOf(userId);
+        if(index !=null){
+            sessions.splice(index, 1);
+        } else {
+            return null;
+        }
+    },
+
+    getSessionById: function(userId) {
+        var index = this.indexOfUser(userId);
+        if(index != null) {
+            return sessions[index];
+        } else {
+            return null;
+        }
+    },
+
+    getSessionByUserId: function(sessionId) {
+        var index = this.indexOfUser(userId);
+        if(index != null) {
+            return sessions[index];
+        } else {
+            return null;
+        }
+    },
 };
 
 module.exports = sessionController;
